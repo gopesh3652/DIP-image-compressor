29197c913a76c6154e4989aed67e3a4860882035
gopesh3652 <gopeshkhandelwal10@gmail.com>
2024-05-28 19:58:43 +0530

    improved compression algo

--- compress_image.py
+++ compress_image.py
@@ -1,6 +1,6 @@
 import cv2
 
-def compress_image(input_path, output_path, quality=95):
+def compress_image(input_path, output_path, quality=45):
     image = cv2.imread(input_path)
     encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), quality]
     cv2.imwrite(output_path, image, encode_param)
